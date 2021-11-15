f478179ea0e0c6bda21ce6eeb3ae9ee5385a4c6a
nikitonya <nik_osipov_2014@mail.ru>
2021-11-15 14:21:57 +0400

    create first window of app

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# Piano on Python
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+def application():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+
+    window.setWindowTitle("Пианино")
+    window.setGeometry(300, 250, 350, 200)
+
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    application()
